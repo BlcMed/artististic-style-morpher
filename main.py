5d9d650c5a7a47cbbbbed1e95f556138c1e9eea8
BlcMed <boulaich.mohamed970@gmail.com>
2024-10-10 16:34:32 +0100

    feat: add toggle option to choose an existing image as a style reference

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from src.image_generation import reconstruct_image_from_content_style
 from src.image_operations import read_image, image_to_tensor, tensor_to_image
 from src.model_utils import load_model
 from config import config
+from src.data_manager import get_files
 
+model_config = config.get("model")
+paths_config = config.get("paths")
 streamlit_config = config.get("streamlit")
 st.title(streamlit_config["title"])
 st.write(streamlit_config["description"])
@@ -13,6 +16,7 @@ if "model" not in st.session_state:
     st.session_state["model"] = vgg_model
     st.session_state["transform"] = transform
 
+default_style_images = get_files(paths_config["style_references"])
 col1, col2 = st.columns(2)
 
 with col1:
@@ -24,17 +28,27 @@ with col1:
         st.image(content_image, use_column_width=True)
 
 with col2:
-    style_image_path = st.file_uploader(
-        "Upload an image as style reference", type=["jpg"]
-    )
-    if style_image_path:
+    default = streamlit_config["default_style_messsage"]
+    upload = streamlit_config["upload_style_message"]
+    style_option = st.radio("Choose an option", [default, upload])
+    if style_option == upload:
+        style_image_path = st.file_uploader(
+            "Upload an image as style reference", type=["jpg"]
+        )
+        if style_image_path:
+            style_image = read_image(style_image_path)
+            st.image(style_image, use_column_width=True)
+    else:
+        selected_style_image = st.selectbox(
+            "Select a default style image", list(default_style_images.keys())
+        )
+        style_image_path = default_style_images[selected_style_image]
         style_image = read_image(style_image_path)
         st.image(style_image, use_column_width=True)
 
+
 if st.button(streamlit_config["transfer_style_button"]):
     if content_image and style_image:
-        model_config = config.get("model")
-
         model = st.session_state.model
         transform = st.session_state.transform
 
