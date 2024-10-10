f79d9e1dd4d639acfaa7e93e3e9704191261d185
BlcMed <boulaich.mohamed970@gmail.com>
2024-10-10 15:40:03 +0100

    feat: initialize configuration files

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import streamlit as st
 from src.image_generation import reconstruct_image_from_content_style
 from src.image_operations import read_image, image_to_tensor, tensor_to_image
 from src.model_utils import load_model
+from config import config
 
-st.title("Articstic Style Morpher")
+streamlit_config = config.get("streamlit")
+st.title(streamlit_config["title"])
+st.write(streamlit_config["description"])
 
 if "model" not in st.session_state:
     vgg_model, transform = load_model()
@@ -28,8 +31,10 @@ with col2:
         style_image = read_image(style_image_path)
         st.image(style_image, use_column_width=True)
 
-if st.button("transfer style", help="Apply the style to the content image"):
+if st.button(streamlit_config["transfer_style_button"]):
     if content_image and style_image:
+        model_config = config.get("model")
+
         model = st.session_state.model
         transform = st.session_state.transform
 
@@ -39,16 +44,21 @@ if st.button("transfer style", help="Apply the style to the content image"):
             new_image_t = reconstruct_image_from_content_style(
                 content_image_t=content_image_t,
                 style_image_t=style_image_t,
-                content_layer=0,
-                style_layers=list(range(4)),
+                content_layer=model_config["content_layer"],
+                style_layers=model_config["style_layers"],
                 model=model,
                 transform=transform,
-                content_weight=0.97,
-                num_iteration=300,
-                learning_rate=0.01,
+                generated_image_resolution=model_config["generated_image_resolution"],
+                content_weight=model_config["content_weight"],
+                num_iteration=model_config["num_iteration"],
+                learning_rate=model_config["learning_rate"],
             )
             new_image = tensor_to_image(new_image_t)
 
-        st.image(new_image, caption="Result Image", use_column_width=True)
+        st.image(
+            new_image,
+            caption=streamlit_config["result_image_caption"],
+            use_column_width=True,
+        )
     else:
         st.warning("Please upload both a content image and a style image.")
